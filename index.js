1ba907dd5046c10853ddb69d044ba92f72883a7a
olivierrr <oliviersinbox@gmail.com>
2014-08-13 17:10:14 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,91 @@
+var THREE = require('n3d-threejs')
+,   OrbitControls = require('./modules/OrbitControls')(THREE)
+,   AnimationFrame = require('animation-frame').shim()
+,   matrix_3d = require('matrix-3d')
+,   Utils = require('./modules/utils')
+,   Particle = require('./modules/particle')
+
+var container, stats;
+var camera, scene, renderer, particles, geometry, material, h, color;
+var mouseX = 0, mouseY = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+var controls;
+
+// css 'reset'
+document.body.style.margin = 0;
+document.body.style.overflow = "hidden";
+
+init();
+animate();
+
+function init() {
+
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 3000 );
+	camera.position.z = 1000;
+
+	controls = new THREE.OrbitControls( camera );
+
+	scene = new THREE.Scene();
+	scene.fog = new THREE.FogExp2( 0x000000, 0.0009 );
+
+	geometry = new THREE.Geometry();
+
+	for ( var i = 0; i < 50000; i ++ ) {
+		var vertex = new THREE.Vector3();
+		vertex.x = Math.random() * 2000 - 1000;
+		vertex.y = Math.random() * 2000 - 1000;
+		vertex.z = Math.random() * 2000 - 1000;
+
+		geometry.vertices.push( vertex );
+	}
+
+
+	size  = 5
+	material = new THREE.PointCloudMaterial( { size: size } );
+	particles = new THREE.PointCloud( geometry, material);
+	scene.add( particles );
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container.appendChild( renderer.domElement );
+
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+	mouseX = event.clientX - windowHalfX;
+	mouseY = event.clientY - windowHalfY;
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+	render();
+}
+
+function render() {
+
+	renderer.render( scene, camera );
+}
